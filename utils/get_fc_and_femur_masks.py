77065529aeb78a417a279c361492e6cb393ac31b
Anoosha Pai <anoopai@stanford.edu>
2025-01-15 17:15:03 -0800

    added' get_fc_mask and get_fc_and_femur_masks' code to utils

---
+++ utils/get_fc_and_femur_masks.py
@@ -0,0 +1,24 @@
+def get_fc_and_femur_masks(seg_path, seg_fc&femur_path):
+    
+    '''
+    Get femoral cartilage label from the segmentation
+    
+    seg_path: str
+        Path to the segmentation file
+    seg_fc_path: str
+        Path to save the femoral cartilage segmentation file 
+    '''
+
+    import os
+    import nibabel as nib
+    import numpy as np
+
+    if os.path.exists(seg_path):
+        seg = nib.load(seg_path)
+        # fc_seg = np.where(seg.get_fdata() == 2, 1, 0).astype(float)
+        fc_seg = np.where(np.isin(seg.get_fdata(), [2, 7]), seg.get_fdata(), 0).astype(float) # get femur segmentation
+        seg_nii = nib.Nifti1Image(fc_seg, seg.affine)
+        print(f'Saving femoral cartilage segmentation to {seg_fc&femur_path}')
+        nib.save(seg_nii, seg_fc&femur_path)
+    else:
+        print(f'{seg_path} does not exist. Please check the path.')
--- utils/get_fc_mask.py
+++ utils/get_fc_mask.py
@@ -1,4 +1,4 @@
-def get_fc_label(seg_path, seg_fc_path):
+def get_fc_mask(seg_path, seg_fc_path):
     
     '''
     
@@ -11,15 +11,16 @@ def get_fc_label(seg_path, seg_fc_path):
     
     '''
 
-    import osi
+    import os
     import nibabel as nib
+    import numpy as np
 
     if os.path.exists(seg_path):
         seg = nib.load(seg_path)
         fc_seg = np.where(seg.get_fdata() == 2, 1, 0).astype(float)
     #    fc_seg = np.where(np.isin(seg.get_fdata(), [2, 7]), seg.get_fdata(), 0).astype(float) # get femur segmentation
         seg_nii = nib.Nifti1Image(fc_seg, seg.affine)
-        print(f'Saving {seg_save_path}')
+        print(f'Saving femoral cartilage segmentation to {seg_fc_path}')
         nib.save(seg_nii, seg_fc_path)
     else:
         print(f'{seg_path} does not exist. Please check the path.')
